cca9c1d34944da111e5325b9fdefbdb0a165e274
saibot777 <stefantrajkovic777@gmail.com>
2019-03-26 14:07:19 +0100

    run analyses method update

--- Kits/plinko/score.js
+++ Kits/plinko/score.js
@@ -1,29 +1,35 @@
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel])
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
-    .sortBy(row => row[0])
-    .slice(0, k)
-    .countBy(row => row[1])
-    .toPairs()
-    .sortBy(row => row[1])
-    .last()
-    .first()
-    .parseInt()
-    .value();
+  const [testSet, trainingSet] = splitDataSet(outputs, 10);
 
-  console.log('Your point will probably fall into ', bucket);
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0])
+    console.log(bucket)
+  }
+}
+
+function knn(data, point) {
+  return _.chain(data)
+  .map(row => [distance(row[0], point), row[3]])
+  .sortBy(row => row[0])
+  .slice(0, k)
+  .countBy(row => row[1])
+  .toPairs()
+  .sortBy(row => row[1])
+  .last()
+  .first()
+  .parseInt()
+  .value();
 }
 
 function splitDataSet(data, testCount) {
